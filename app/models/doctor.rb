21089cad5cf8a8bc53186d59703c979b9a00056d
Eytan Anjel <anjel@usc.edu>
2013-12-21 15:18:15 +0200

    mass assing permitted for necessary fields

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -30,6 +30,6 @@ class Doctor < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :tc_no
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :tc_no, :first_name, :last_name, :phone, :specialization
   
 end

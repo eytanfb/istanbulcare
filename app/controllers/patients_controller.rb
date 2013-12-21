bd7e029121c4efa1f99e20c733ba689a7b9a28b2
Eytan Anjel <anjel@usc.edu>
2013-12-21 18:12:52 +0200

    patients show added

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,13 @@
-class PatientsController < ApplicationController
+class PatientsController < Devise::RegistrationsController
+  
+  def show
+    @patient = Patient.find params[:id]
+  end
+  
+  private
+  
+  def after_sign_in_path_for(resource)
+    current_patient_path
+  end
+  
 end

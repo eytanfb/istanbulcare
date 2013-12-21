f9738a9281f892cedbbeac2aa2e2eb1de123dd1c
Eytan Anjel <anjel@usc.edu>
2013-12-22 01:57:07 +0200

    prescriptions can be seen on the patient side

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -2,12 +2,7 @@ class PatientsController < Devise::RegistrationsController
   
   def show
     @patient = Patient.find params[:id]
-  end
-  
-  private
-  
-  def after_sign_in_path_for(resource)
-    current_patient_path
+    @prescriptions = @patient.prescriptions
   end
   
 end

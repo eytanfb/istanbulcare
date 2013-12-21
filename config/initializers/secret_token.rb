7c8813333640fbc540769c77965808e47231f493
Eytan Anjel <anjel@usc.edu>
2013-12-21 13:01:55 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Istanbulcare::Application.config.secret_token = 'f59d355c39eec6f8004bb159d8494fe493ea5a7a0350193513d75c2c3f0b8a64ba476bd1a532ca3ad8383559fbe157620cff0bd49bb084f116e737b0bf10c9c2'

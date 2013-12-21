2cecad0e7341447101cf5b1c1d2fad17521eee0a
Eytan Anjel <anjel@usc.edu>
2013-12-21 19:58:16 +0200

    after sign in path fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def after_sign_in_path_for(resource)
+    case resource
+    when Patient then patient_path(resource)
+    when Doctor then doctor_path(resource)
+    end
+  end
 end

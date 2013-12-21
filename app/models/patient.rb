33c308cba3ddf4086cc997da36dd6afe06633929
Eytan Anjel <anjel@usc.edu>
2013-12-21 17:54:48 +0200

    mass assignment added to patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: patients
+#
+#  id                     :integer          not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0)
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  first_name             :string(255)
+#  last_name              :string(255)
+#  tc_no                  :integer
+#  phone                  :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
 class Patient < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -6,6 +29,11 @@ class Patient < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :tc_no, :phone
+  
+  validates_length_of :tc_no, :within => 11..11, :on => :create, :message => "11 Hane Olmalidir"
+  
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end

4ce6843f40a5ac1e67d6c69fe3254699cf2ceb6d
Eytan Anjel <anjel@usc.edu>
2013-12-22 02:07:47 +0200

    still adding visuals

--- test/unit/visual_test.rb
+++ test/unit/visual_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: visuals
+#
+#  id         :integer          not null, primary key
+#  patient_id :integer
+#  type       :string(255)
+#  image_url  :string(255)
+#  date       :date
+#  notes      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class VisualTest < ActiveSupport::TestCase

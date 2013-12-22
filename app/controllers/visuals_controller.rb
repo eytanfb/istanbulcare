39595847137d8291fe218283bebd1c540e27f790
Eytan Anjel <anjel@usc.edu>
2013-12-22 10:30:36 +0200

    conflict resolve

--- app/controllers/visuals_controller.rb
+++ app/controllers/visuals_controller.rb
@@ -3,5 +3,25 @@ class VisualsController < ApplicationController
   def show
     @visual = Visual.find params[:id]
   end
+  
+  def new
+    @patient = Patient.find_by_tc_no params[:patient_tc_no]
+    @visual = @patient.visuals.new
+  end
+  
+  def create
+    @patient = Patient.find params[:patient_id]
+    @visual = @patient.visuals.new params[:visual]
+    
+    respond_to do |format|
+      if @visual.save
+        format.html { redirect_to(@visual, :notice => 'Gorsel basariyla olusturuldu.') }
+        format.xml  { render :xml => @visual, :status => :created, :location => @visual }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @visual.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Istanbulcare::Application.routes.draw do
 
-  get "operations/show"
-
   get "static_pages/home"
 
   devise_for :doctors
@@ -18,6 +16,7 @@ Istanbulcare::Application.routes.draw do
   end
   
   resources :prescriptions
+  resources :visuals
   match 'visuals/:id' => "visuals#show", as: :visual
   match "vaccines/:id" => "vaccines#show", as: :vaccine
   match "operations/:id" => "operations#show", as: :operation

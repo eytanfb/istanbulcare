2b379bd1157cfa206c290884b7f88f5a1cd6f52e
Eytan Anjel <anjel@usc.edu>
2013-12-22 02:17:49 +0200

    added visuals to patient and created views for visuals

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -3,6 +3,7 @@ class PatientsController < Devise::RegistrationsController
   def show
     @patient = Patient.find params[:id]
     @prescriptions = @patient.prescriptions
+    @visuals = @patient.visuals
   end
   
 end
--- app/models/visual.rb
+++ app/models/visual.rb
@@ -15,4 +15,9 @@
 class Visual < ActiveRecord::Base
   belongs_to :patient
   attr_accessible :date, :image_url, :notes, :type
+  
+  def date
+    self.date.strftime("%d/%m/%Y")
+  end
+  
 end

9b4f023a1d298f74dc021eab8a7eca6540605920
Eytan Anjel <anjel@usc.edu>
2013-12-22 03:40:17 +0200

    commit

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -4,4 +4,9 @@ class PrescriptionsController < ApplicationController
     @prescription = Prescription.find params[:id]
   end
   
+  def new
+    @prescription = current_doctor.prescription.new
+    @patient_tc_no = params[:patient_tc_no]
+  end
+  
 end
